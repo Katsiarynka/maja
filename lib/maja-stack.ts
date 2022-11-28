a070ceb9d8872955292fe6719f9796510bec239b
Kate Yanchenko <kate.yanchenka@cervest.earth>
2022-11-28 15:43:14 -0800

    Add python image and infra for that

--- lib/maja-stack.ts
+++ lib/maja-stack.ts
@@ -1,19 +1,18 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
 
 export class MajaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'MajaQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    
+    // Create ECS cluster with python3.9 image
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      clusterName: 'maja-cluster',
+      containerInsights: true,
+      defaultCloudMapNamespace: {
+        name: 'maja-namespace',
+      },
     });
-
-    const topic = new sns.Topic(this, 'MajaTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+    }
 }
